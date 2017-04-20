edc34b0d09d44b2f70c5ccd9f3cfe332bcba6454
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-04-21 00:44:55 +0500

    fix local host

--- server.js
+++ server.js
@@ -2,18 +2,18 @@ var express = require("express");
 
 //create our app
 var app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
   if (req.headers["x-forwarded-proto"] === "http") {
-    next();
-  } else {
     res.redirect("https://" + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
 app.use(express.static("public"));
 
 app.listen(PORT, function (){
-    console.log("Express server is up on the port" + PORT);
+    console.log("Express server is up on the port " + PORT);
 });
